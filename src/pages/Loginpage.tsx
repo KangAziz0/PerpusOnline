2394b348776ebc47be219c260a62457d55ff64c3
AbdulAziz <muhamadazizabdulaziz35@gmail.com>
2024-07-20 21:08:29 +0700

    add validation login

--- src/pages/Loginpage.tsx
+++ src/pages/Loginpage.tsx
@@ -10,35 +10,60 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+const loginFormSchema = z.object({
+    username: z.string().min(6, "Username Minimal 6 Karakter").max(16, "Username Maximal 16 Karakter"),
+    password: z.string().min(8, "Password Minimal 8 Karakter")
+})
+
+type LoginFormSchema = z.infer<typeof loginFormSchema>
+
 export function LoginPage() {
+    const { register, handleSubmit, formState } = useForm<LoginFormSchema>({
+        resolver: zodResolver(loginFormSchema)
+    });
+
+    const onSubmit = handleSubmit((values) => {
+        alert(`username : ${values.username} || password ${values.password}`)
+    });
     return (
         <AuthLayout>
             <Card className="w-full max-w-sm">
-                <CardHeader>
-                    <CardTitle className="text-blue-700 font-bold text-4xl">Login</CardTitle>
-                    <CardDescription>Please enter your credentials</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
+                <form onSubmit={onSubmit}>
+                    <CardHeader>
+                        <CardTitle className="text-blue-700 font-bold text-4xl">Login</CardTitle>
+                        <CardDescription>Please enter your credentials</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
-                                <Label htmlFor="email">Email</Label>
-                                <Input id="email" placeholder="example@example.com" typeof="email" />
+                                <Label htmlFor="username">Username</Label>
+                                <Input id="username" placeholder="Ganjar Sugianto" typeof="text" {...register("username")} />
+                                {formState.errors.username && (
+                                    <p className="text-red-500">{formState.errors.username.message}</p>
+                                )}
                             </div>
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="password">Password</Label>
-                                <Input id="password" placeholder="********" typeof="password" />
+                                <Input id="password" placeholder="********" typeof="password" {...register("password")} />
+                                {formState.errors.password && (
+                                    <p className="text-red-500">{formState.errors.password.message}</p>
+                                )}
                             </div>
                         </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <Button className="w-full" variant="default">Login</Button>
-                </CardFooter>
-                <p className="flex justify-center gap-2 mb-4">Don't Have a account ? <Link to="/register" className="text-blue-700 font-bold">Sign Up</Link> </p>
+
+                    </CardContent>
+                    <CardFooter>
+                        <Button className="w-full" variant="default" type="submit">Login</Button>
+                    </CardFooter>
+                    <p className="flex justify-center gap-2 mb-4">Don't Have a account ? <Link to="/register" className="text-blue-700 font-bold">Sign Up</Link> </p>
+                </form>
             </Card>
-        </AuthLayout>
+        </AuthLayout >
     )
 }
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -13,17 +13,30 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 import { Link } from "react-router-dom"
+import { useForm } from "react-hook-form"
+import {z} from "zod"
+
+const registerFormSchema = z.object({
+    fullname:z.string().min(6,"min 6 karakter").max(16,"max 16 karakter"),
+    email:z.string().email("harus format Email"),
+    password : z.string().min(8,"min 8 karakter"),
+    // confirm:z.string().
+})
+
+type RegisterFormSchema = z.infer<typeof registerFormSchema>
 
 export function RegisterPage() {
+    const form = useForm<RegisterFormSchema>()
     return (
         <AuthLayout>
             <Card className="w-full max-w-sm">
-                <CardHeader>
-                    <CardTitle className="text-blue-700 font-bold text-4xl">Register</CardTitle>
-                    <CardDescription>Please enter your credentials</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <form>
+                <form>
+                    <CardHeader>
+                        <CardTitle className="text-blue-700 font-bold text-4xl">Register</CardTitle>
+                        <CardDescription>Please enter your credentials</CardDescription>
+                    </CardHeader>
+                    <CardContent>
+
                         <div className="grid w-full items-center gap-4">
                             <div className="flex flex-col space-y-1.5">
                                 <Label htmlFor="email">Fullname</Label>
@@ -42,13 +55,13 @@ export function RegisterPage() {
                                 <Input id="confirm" placeholder="********" typeof="password" />
                             </div>
                         </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex justify-between">
-                    <Button className="w-full" variant="default">Register</Button>
-                </CardFooter>
-                <p className="flex justify-center gap-2 mb-4">Already have account ? <Link to="/login" className="text-blue-700 font-bold">Sign In</Link> </p>
+                    </CardContent>
+                    <CardFooter>
+                        <Button className="w-full" variant="default" type="submit">Register</Button>
+                    </CardFooter>
+                    <p className="flex justify-center gap-2 mb-4">Already have account ? <Link to="/login" className="text-blue-700 font-bold">Sign In</Link> </p>
+                </form>
             </Card>
-        </AuthLayout>
+        </AuthLayout >
     )
 }
